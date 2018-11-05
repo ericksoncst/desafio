f03c67fd44a361838683086ac9804dafcc58930c
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-11-05 11:17:51 -0200

    crud autores completo

--- routes/api/autores.js
+++ routes/api/autores.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+const mongoose = require('mongoose');
 
 //load validation
 const validateRegisterInput = require('../../validation/cadastro');
@@ -13,6 +14,8 @@ const validateLoginInput = require('../../validation/login');
  //load autor model
 const Autor = require('../../models/Autor');
 
+//@route http://localhost:5000/api/autores/cadastro
+//POST cadastro do autor
 router.post('/cadastro' ,(req, res)=> {
 
     const {errors, isValid} = validateRegisterInput(req.body);
@@ -48,7 +51,8 @@ router.post('/cadastro' ,(req, res)=> {
     } );
 });
 
-//login
+//@route http://localhost:5000/api/autores/login
+//POST login do autor
 router.post('/login', (req,res) => {
 
     const {errors, isValid} = validateLoginInput(req.body);
@@ -90,6 +94,68 @@ router.post('/login', (req,res) => {
             }
         });
     });
+});
+
+//@route http://localhost:5000/api/autores
+//GET listando autores/ opção de paginação
+router.get('/', async (req, res) => {
+	try{
+		const { pagina, porPagina} = req.query;
+		const opcoes = {
+			page: parseInt(pagina, 10) || 1,
+			limit: parseInt(porPagina, 10) || 10,
+		}
+		const autores = await Autor.paginate({}, opcoes);
+		return res.json(autores);
+	} catch (err){
+		console.error(err);
+		return res.status(500).send(err);
+	}
+});
+
+//@route http://localhost:5000/api/autores/atual
+//GET autor logado
+router.get('/atual', 
+passport.authenticate('jwt', 
+{ session: false}), 
+(req, res) => {
+    //res.json({msg: 'success'});
+    res.json({
+        id: req.user.id,
+        nome: req.user.nome,
+        email: req.user.email,
+    });
+});
+
+//@route http://localhost:5000/api/autores/:id
+//PUT atualizando autor
+router.put('/:id' , (req, res) => {
+    const id = req.params.id;
+    Autor.update(
+        { '_id': id },
+        { $set:  { 'nome': req.body.nome }},
+        (err, result) => {
+          if (err) {
+            res.status(500)
+            .json({ error: 'Não foi possivel atualizar autor'});
+          } else {
+            res.status(200)
+            .json(result);
+          }
+       }
+      );
+});
+
+
+//@route http://localhost:5000/api/autores/:id
+//DELETE deletando autor
+router.delete('/:id',(req, res) => {
+    const id = req.params.id;
+    Autor.findById({_id: id})
+    .then(autor => {
+    autor.remove().then(() => res.json({ msg: "Autor excluido com sucesso" }));
+    })
+    .catch(err => res.status(404).json({ msg: 'Autor não encontrado' }));
 });
 
 
